2f134aa6311c11c9e64d7b6a2ec6b0be9416f212
03poverty-slugs@icloud.com <gotteeth>
2024-04-09 22:25:15 -0400

    first part of lession 2

--- task-3.py
+++ task-3.py
@@ -4,4 +4,6 @@ year = int(input("enter a year: "))
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0): #still dont understand this 
     print(" year " , + year + "is a leap year.")
 else:
-    print("this is not a leap year.")
+    print("this is not a leap year.") 
+
+    #had to revisit old math 
